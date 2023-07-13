d6fe8c77774a5fcbf3ea6bc73ba877cdf66b1d97
Sendeky <spruslasha@gmail.com>
2023-07-12 23:21:41 -0700

    intial commit

---
+++ MacOS-MenuBar-Calculator/MacOS-MenuBar-Calculator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacOS-MenuBar-Calculator
+//
+//  Created by Ruslan Spirkin on 7/12/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
