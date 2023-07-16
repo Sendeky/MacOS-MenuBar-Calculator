e237de4986fbdcd1fae0aa20f4caf792286acff9
Sendeky <spruslasha@gmail.com>
2023-07-15 20:56:06 -0700

    connected buttons to functions, added keys to them

--- MacOS-MenuBar-Calculator/MacOS-MenuBar-Calculator/CalculatorController.swift
+++ MacOS-MenuBar-Calculator/MacOS-MenuBar-Calculator/CalculatorController.swift
@@ -9,6 +9,18 @@ import Cocoa
 
 class CalculatorController: NSViewController {
 
+    @IBOutlet weak var Button1: RoundedColoredButton!
+    @IBOutlet weak var Button2: RoundedColoredButton!
+    @IBOutlet weak var Button3: RoundedColoredButton!
+    @IBOutlet weak var Button4: RoundedColoredButton!
+    @IBOutlet weak var Button5: RoundedColoredButton!
+    @IBOutlet weak var Button7: RoundedColoredButton!
+    @IBOutlet weak var Button6: RoundedColoredButton!
+    @IBOutlet weak var Button8: RoundedColoredButton!
+    @IBOutlet weak var Button9: RoundedColoredButton!
+    @IBOutlet weak var Button0: RoundedColoredButton!
+    
+    // Operator buttons
     @IBOutlet weak var MinusButton: NSButton!
     @IBOutlet weak var PlusButton: NSButton!
     @IBOutlet weak var TopLabelTextField: NSTextField!
@@ -23,6 +35,45 @@ class CalculatorController: NSViewController {
         
         PlusButton.target = self
         PlusButton.action = #selector(plusTapped)
-
+        
+        // B1
+        Button1.target = self
+        Button1.action = #selector(button1Tapped)
+        
+        // B2
+        Button2.target = self
+        Button2.action = #selector(button2Tapped)
+        
+        // B3
+        Button3.target = self
+        Button3.action = #selector(button3Tapped)
+        
+        // B4
+        Button4.target = self
+        Button4.action = #selector(button4Tapped)
+        
+        // B5
+        Button5.target = self
+        Button5.action = #selector(button5Tapped)
+        
+        // B6
+        Button6.target = self
+        Button6.action = #selector(button6Tapped)
+        
+        // B7
+        Button7.target = self
+        Button7.action = #selector(button7Tapped)
+        
+        // B8
+        Button8.target = self
+        Button8.action = #selector(button8Tapped)
+        
+        // B9
+        Button9.target = self
+        Button9.action = #selector(button9Tapped)
+        
+        // B0
+        Button0.target = self
+        Button0.action = #selector(button0Tapped)
     }
 }
--- MacOS-MenuBar-Calculator/MacOS-MenuBar-Calculator/ExtensionFunctions.swift
+++ MacOS-MenuBar-Calculator/MacOS-MenuBar-Calculator/ExtensionFunctions.swift
@@ -21,8 +21,45 @@ extension CalculatorController {
         TopLabelTextField.stringValue = "plus pressed"
     }
     
-    // for when numbers entered
-    @objc func numbersEntered(Input: Float ) {
-        print("numbersEntered!")
+    //Amazing code
+    // for when 1 tapped
+    @objc func button1Tapped() {
+        print("1 tapped")
+    }
+    // for when 2 tapped
+    @objc func button2Tapped() {
+        print("2 tapped")
+    }
+    // for when 3 tapped
+    @objc func button3Tapped() {
+        print("3 tapped")
+    }
+    // for when 4 tapped
+    @objc func button4Tapped() {
+        print("4 tapped")
+    }
+    // for when 5 tapped
+    @objc func button5Tapped() {
+        print("5 tapped")
+    }
+    // for when 6 tapped
+    @objc func button6Tapped() {
+        print("6 tapped")
+    }
+    // for when 7 tapped
+    @objc func button7Tapped() {
+        print("7 tapped")
+    }
+    // for when 8 tapped
+    @objc func button8Tapped() {
+        print("8 tapped")
+    }
+    // for when 9 tapped
+    @objc func button9Tapped() {
+        print("9 tapped")
+    }
+    // for when 0 tapped
+    @objc func button0Tapped() {
+        print("0 tapped")
     }
 }
